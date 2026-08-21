df8b94fe92fba895d74d90cf0f41056a27fe6d11
mateofoulkes-lab <mateofoulkes@gmail.com>
2026-08-21 17:37:44 -0300

    Bump service worker cache to v0.10.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pirulin-pwa-v0.10.3-dev';
+const CACHE = 'pirulin-pwa-v0.10.4-dev';
 const CORE = [
   './','./index.html','./mockup_pirulin_v51.html','./firebase-client.js',
   './tasks-repository.js','./tasks-preferences.js','./tasks-advanced.js','./tasks-live-adapter.js',

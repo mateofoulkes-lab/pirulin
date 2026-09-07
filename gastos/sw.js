1aa4ff48bd1ab2ed021110a8dc76568522914beb
mateofoulkes-lab <mateofoulkes@gmail.com>
2026-09-07 16:40:39 -0300

    Bump Gastos cache to v7

--- gastos/sw.js
+++ gastos/sw.js
@@ -1,4 +1,4 @@
-const CACHE='pirulin-gastos-v6';
+const CACHE='pirulin-gastos-v7';
 const CORE=['./','./index.html','./standalone.css','./standalone.js','./manifest.webmanifest','../pirulin-icon.svg','../LOGO PIRULIN.png','../firebase-client.js','../expenses-repository.js','../expenses-live.js','../expenses-final-fixes.js','../expenses-ui-polish.js','../expenses-percent-slider.js','../expenses-categories.js','../expenses-card-compact.js','../expenses-report.js','../expenses-report-motion.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
